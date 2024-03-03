ff715c34e07a2f01c86cb0756d41cba10ae92f72
oabramchik <oabramchik@gmail.com>
2024-03-03 15:38:55 +0300

    homework 4 olga_abramchik done

---
+++ homework/olga_abramchik/homework_4/homework_4.py
@@ -0,0 +1,14 @@
+my_dict = dict()
+my_dict['tuple'] = (1, 56, 3.23, "text", 6) 
+my_dict['list'] = [56, 42, 102, 63, 69] 
+my_dict['dict'] = {'num1': 1, 'num2': 2, 'num3': 3, 'num4': 4, 'num5': 5}
+my_dict['set'] = {1, 2, 3, 8, 9}
+last_tuple = my_dict['tuple'][-1]
+print('Последний элемент ключа tuple: ', last_tuple)
+my_dict['list'].append('list_new')
+my_dict['list'].pop(1)
+my_dict['dict']['i am a tuple'] = 'value'
+my_dict['dict'].pop('num1')
+my_dict['set'].add('new_set')
+my_dict['set'].pop()
+print(my_dict)
