f22c6149205836115ec390c726950f5c23c38564
oabramchik <oabramchik@gmail.com>
2024-03-03 15:51:02 +0300

    fixed formatting

--- homework/olga_abramchik/homework_4/homework_4.py
+++ homework/olga_abramchik/homework_4/homework_4.py
@@ -1,6 +1,6 @@
 my_dict = dict()
-my_dict['tuple'] = (1, 56, 3.23, "text", 6) 
-my_dict['list'] = [56, 42, 102, 63, 69] 
+my_dict['tuple'] = (1, 56, 3.23, "text", 6)
+my_dict['list'] = [56, 42, 102, 63, 69]
 my_dict['dict'] = {'num1': 1, 'num2': 2, 'num3': 3, 'num4': 4, 'num5': 5}
 my_dict['set'] = {1, 2, 3, 8, 9}
 last_tuple = my_dict['tuple'][-1]
