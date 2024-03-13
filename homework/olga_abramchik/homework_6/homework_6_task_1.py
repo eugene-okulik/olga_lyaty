d47e11df8a274422a073f6a53232423d8ae9cb52
oabramchik <oabramchik@gmail.com>
2024-03-13 23:36:11 +0300

    fixed formating in file task1.py

--- homework/olga_abramchik/homework_6/homework_6_task_1.py
+++ homework/olga_abramchik/homework_6/homework_6_task_1.py
@@ -1,5 +1,5 @@
 text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
-'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
+        'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
 text = text.split()
 
 for i in range(len(text)):
