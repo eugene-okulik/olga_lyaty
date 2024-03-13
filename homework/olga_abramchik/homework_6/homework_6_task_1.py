192bea05b4426c87800b842ac270a08d0dd0b7e6
oabramchik <oabramchik@gmail.com>
2024-03-13 23:15:52 +0300

    homework 6 olga abramchik done

---
+++ homework/olga_abramchik/homework_6/homework_6_task_1.py
@@ -0,0 +1,11 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+text = text.split()
+
+for i in range(len(text)):
+    if text[i][-1] in ',.':
+        text[i] = text[i][0:-1] + 'ing' + text[i][-1]
+    else:
+        text[i] += 'ing'
+
+text_new = ' '.join(text)
+print(text_new)
