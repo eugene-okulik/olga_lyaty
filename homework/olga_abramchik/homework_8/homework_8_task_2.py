7731fb09112b59ea3a88d2137f65e70f5461f165
oabramchik <oabramchik@gmail.com>
2024-03-25 01:07:45 +0300

    homework 8 done olga abramchik

---
+++ homework/olga_abramchik/homework_8/homework_8_task_2.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.set_int_max_str_digits(0)
+
+
+def fibonacci_gen():
+    f0, f1 = 0, 1
+    while True:
+        yield f0
+        f0, f1 = f1, f0 + f1
+
+f = fibonacci_gen()
+count = 0
+while True:
+    count += 1
+    f_number = next(f)
+    if count == 5:
+        print(f'5-ое чиcло Фибоначчи: {f_number}')
+    elif count == 200:
+        print(f'200-ое чиcло Фибоначчи: {f_number}')
+    elif count == 1000:
+        print(f'1 000-ое чиcло Фибоначчи: {f_number}')
+    elif count == 100000:
+        print(f'100 000-ое чиcло Фибоначчи: {f_number}')
+        break
